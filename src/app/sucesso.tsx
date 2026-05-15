7fffa2e145f32053c98456be8842abdc2c5c354c
Daniel Akiama <danielakiamaoficial@gmail.com>
2026-05-15 19:08:18 -0400

    inicialização da estrutura base

---
+++ src/app/sucesso.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function SucessoScreen(){
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Sucesso Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
